899b9506f55d68e96d3cc44862827a98016d1cf7
Ramón Arteaga <dev.rarteaga@gmail.com>
2023-02-27 18:22:27 +0100

    closes #4 Used OnPush as detection strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component, EnvironmentInjector } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EnvironmentInjector } from '@angular/core';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 @Component({
+  // +--------------------+
+  // |                    |
+  // |  COMPONENT CONFIG  |
+  // |                    |
+  // +--------------------+
   standalone: true,
   imports: [IonicModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
+// +-------------------+
+// |                   |
+// |  COMPONENT LOGIC  |
+// |                   |
+// +-------------------+
 export class AppComponent {
   constructor(public environmentInjector: EnvironmentInjector) { }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
+  // +--------------------+
+  // |                    |
+  // |  COMPONENT CONFIG  |
+  // |                    |
+  // +--------------------+
   standalone: true,
   imports: [
     CommonModule,
     IonicModule
   ],
   selector: 'app-home',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+// +-------------------+
+// |                   |
+// |  COMPONENT LOGIC  |
+// |                   |
+// +-------------------+
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
 
 }
