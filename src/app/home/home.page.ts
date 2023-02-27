eb2705a33812c52d5495d3a236ae4e1f7fcfece1
Ramón Arteaga <dev.rarteaga@gmail.com>
2023-02-27 18:48:00 +0100

    closes #1 Added a welcome message

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -32,8 +32,7 @@ import { IonicModule } from '@ionic/angular';
 
     <ion-content>
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <span class="welcome-message">Welcome!</span>
       </div>
     </ion-content>
   `,
@@ -46,31 +45,17 @@ import { IonicModule } from '@ionic/angular';
   styles: [
     `
       #container {
-        text-align: center;
-
         position: absolute;
         left: 0;
         right: 0;
         top: 50%;
         transform: translateY(-50%);
+        text-align: center;
       }
 
-      #container strong {
-        font-size: 20px;
-        line-height: 26px;
-      }
-
-      #container p {
-        font-size: 16px;
-        line-height: 22px;
-
-        color: #8c8c8c;
-
-        margin: 0;
-      }
-
-      #container a {
-        text-decoration: none;
+      .welcome-message {
+        font-weight: bold;
+        font-size: 1.5rem;
       }
     `
   ]
