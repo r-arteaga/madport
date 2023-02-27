ab1a2ec9fb5c010ef039a80bd149093aa9a0ac5e
Ramón Arteaga <dev.rarteaga@gmail.com>
2023-02-27 18:28:43 +0100

    #5 Added inline template to home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,7 +15,28 @@ import { IonicModule } from '@ionic/angular';
   ],
   selector: 'app-home',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  templateUrl: 'home.page.html',
+
+  // +----------------------+
+  // |                      |
+  // |  COMPONENT TEMPLATE  |
+  // |                      |
+  // +----------------------+
+  template: `
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>
+          Home
+        </ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <div id="container">
+        <strong>Ready to create an app?</strong>
+        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      </div>
+    </ion-content>
+  `,
   styleUrls: ['home.page.scss'],
 })
 
