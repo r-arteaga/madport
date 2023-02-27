86ad58c239b3ad24724f628270400d8185f19371
Ramón Arteaga <dev.rarteaga@gmail.com>
2023-02-27 18:18:03 +0100

    closes #7 Modified home page test

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,24 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { RouterTestingModule } from "@angular/router/testing";
 import { HomePage } from './home.page';
 
 describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+  let page: HomePage
+  let fixture: ComponentFixture<HomePage>
+  let cd: ChangeDetectorRef
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [HomePage],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+    TestBed.overrideComponent(HomePage, {
+      set: {
+        imports: [CommonModule, IonicModule, RouterTestingModule],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      }
+    })
 
-    fixture = TestBed.createComponent(HomePage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture = TestBed.createComponent(HomePage)
+    page = fixture.componentInstance
+    cd = fixture.debugElement.injector.get<ChangeDetectorRef>(ChangeDetectorRef)
+    cd.detectChanges()
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  it('should create the page', () => {
+    expect(page).toBeTruthy();
+  })
+})
